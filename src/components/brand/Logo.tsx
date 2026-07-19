8a416f9227186b570806d72369a9f5ef5a3d9552
Focalleap <127777928+Focalleap@users.noreply.github.com>
2026-07-19 15:17:47 +0100

    Update Logo.tsx

--- src/components/brand/Logo.tsx
+++ src/components/brand/Logo.tsx
@@ -1,4 +1,4 @@
-import logo from "@/assets/ride-next-polo-logo.jpg.asset.json";
+import logo from "@/assets/ride-next-polo-logo.PNGn";
 
 export function Logo({ className = "h-10 w-auto" }: { className?: string }) {
   return (
